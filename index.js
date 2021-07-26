5e70b75436ed7b1301f19408d509af6af9bf2a28
mattegan111 <mnegan93@gmail.com>
2021-07-26 17:27:16 +1200

    fixes async bug on load

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ app.get('/contact-me', async (request, response) => {
     response.send( await readFile('./contact-me.html', 'utf8') );
 });
 
-app.get('/404', async (request, response) => {
-    response.send( await readFile('./404.html', 'utf8') );
-});
-
-app.use(function (request, response) {
-    response.status(404).send( await readFile('./404.html', 'utf8') );
+app.use( async (request, response) => {
+    response.status(404).send( await readFile('./404.html', 'utf8'));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
